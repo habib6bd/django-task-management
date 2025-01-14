98adbd3e7ba5b40cb68900c87c267f01ac0a17f4
habib6bd <habib6bd@gmail.com>
2025-01-15 01:30:33 +0600

    Initial setup completed
    	> Add tailwind in project
    	> Module 4 completed

--- tasks/views.py
+++ tasks/views.py
@@ -7,3 +7,15 @@ def manager_dashboard(request):
 
 def user_dashboard(request):
     return render(request, "dashboard/user_dashboard.html")
+
+def test(request):
+    names = ["Mahmud", "Ahamed", "John", "Mr. X"]
+    count = 0
+    for name in names:
+        count += 1
+    context = {
+        "names": names,
+        "age": 23,
+        "count": count
+    }
+    return render(request, 'test.html', context)
