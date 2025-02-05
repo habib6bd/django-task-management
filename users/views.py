298a0beb08cd0e5a65d8a716e6dc0beaf82c36ad
Habibur Rahaman <habibur@quantummethod.org.bd>
2025-02-05 19:46:38 +0600

    Module 10.2 completed

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-# Create your views here.
+def sign_up(request):
+    if request.method == 'GET':
+        form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            confirm_password = form.cleaned_data.get('password2')
+
+            if password == confirm_password:
+                User.objects.create(username = username, password = password)
+            else:
+                print("password are not same")
+        else:
+            print("Form is not valid")
+            
+    return render(request, 'registration/register.html', {"form": form})
