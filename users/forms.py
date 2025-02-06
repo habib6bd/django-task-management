d33860ff8edf032d70921c2b62b1d6b755229a37
Habibur Rahaman <habibur@quantummethod.org.bd>
2025-02-06 14:00:39 +0600

    module 10.5 field non field completed

---
+++ users/forms.py
@@ -0,0 +1,45 @@
+import re
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'password1', 'password2', 'email']
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']: 
+            self.fields[fieldname].help_text = None
+
+class CustomRegistrationForm(forms.ModelForm):
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'password1', 'confirm_password', 'email']
+
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+        errors = []
+
+        if len(password1) < 8:
+            errors.append('Password must be at least 8 character long')
+
+        if "abc" not in password1:
+            errors.append("Password must include abc")
+
+        if errors:
+            raise forms.ValidationError(errors)
+        return password1
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get('password1')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password1 != confirm_password:
+            raise forms.ValidationError("Password do not match")
+        return cleaned_data
--- users/views.py
+++ users/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from users.forms import CustomRegistrationForm
 
 def sign_up(request):
     if request.method == 'GET':
-        form = UserCreationForm()
+        form = CustomRegistrationForm()
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomRegistrationForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            confirm_password = form.cleaned_data.get('password2')
+            # username = form.cleaned_data.get('username')
+            # password = form.cleaned_data.get('password')
+            # confirm_password = form.cleaned_data.get('password2')
 
-            if password == confirm_password:
-                User.objects.create(username = username, password = password)
-            else:
-                print("password are not same")
+            # if password == confirm_password:
+            #     User.objects.create(username = username, password = password)
+            # else:
+            #     print("password are not same")
+            form.save()
         else:
             print("Form is not valid")
             
