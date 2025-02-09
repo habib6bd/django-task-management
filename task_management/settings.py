71939920717279696b587ea189775a0bdbc1bc58
habib6bd <habib6bd@gmail.com>
2025-02-09 11:42:45 +0600

    test

--- task_management/settings.py
+++ task_management/settings.py
@@ -146,3 +146,12 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "habib6bd@gmail.com"
+EMAIL_HOST_PASSWORD = "psuz kagk rwob zpav"
+
+
