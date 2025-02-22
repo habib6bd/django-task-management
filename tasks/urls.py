cb60e0395dbeebdcb08cbeb983964de5de52da44
Habibur Rahaman <habibur@quantummethod.org.bd>
2025-02-22 16:39:56 +0600

    Module-15 completed

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,16 +1,19 @@
-
 from django.urls import path
-from tasks.views import create_task, manager_dashboard, employee_dashboard, view_task, update_task, delete_task, task_details, dashboard, Greetings,HiGreetings, CreateTask
+from tasks.views import manager_dashboard, employee_dashboard, create_task, view_task, update_task, delete_task, task_details, dashboard, Greetings, HiGreetings, HiHowGreetings, CreateTask, ViewProject, TaskDetail, UpdateTask
 
 urlpatterns = [
-path('manager-dashboard/', manager_dashboard, name="manager-dashboard"),
-path('user-dashboard/', employee_dashboard, name="user-dashboard"),
-# path('create-task', create_task, name='create-task'),
-path('create-task', CreateTask.as_view(), name='create-task'),
-path('view-task', view_task),
-path('task/<int:task_id>/details/', task_details, name='task-details'),
-path('update-task/<int:id>/', update_task, name='update-task'),
-path('delete-task/<int:id>', delete_task, name='delete-task'),
-path('dashboard', dashboard, name='dashboard'),
-path('greetings/', HiGreetings.as_view(greetings='Good Day'), name='greetings')
+    path('manager-dashboard/', manager_dashboard, name="manager-dashboard"),
+    path('user-dashboard/', employee_dashboard, name='user-dashboard'),
+    # path('create-task/', create_task, name='create-task'),
+    path('create-task/', CreateTask.as_view(), name='create-task'),
+    # path('view_task/', view_task, name='view-task'),
+    path('view_task/', ViewProject.as_view(), name='view-task'),
+    # path('task/<int:task_id>/details/', task_details, name='task-details'),
+    path('task/<int:task_id>/details/',
+         TaskDetail.as_view(), name='task-details'),
+    # path('update-task/<int:id>/', update_task, name='update-task'),
+    path('update-task/<int:id>/', UpdateTask.as_view(), name='update-task'),
+    path('delete-task/<int:id>/', delete_task, name='delete-task'),
+    path('dashboard', dashboard, name='dashboard'),
+    path('greetings/', HiHowGreetings.as_view(greetings='Hi Good Day!'), name='greetings')
 ]
