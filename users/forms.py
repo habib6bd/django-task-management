cbaaacd64447b16672b1979e016d9894470b7873
Habibur Rahaman <habibur@quantummethod.org.bd>
2025-02-27 15:41:04 +0600

    Module-16 completed 	> Auth view, login view, customizing login view 	> profile view using template view 	> custom template 	> Logout view 	> password reset view 	> password reset confirm view 	> custom email template

--- users/forms.py
+++ users/forms.py
@@ -1,35 +1,40 @@
-import re
 from django import forms
+import re
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User, Permission, Group
 from tasks.forms import StyledFormMixin
-from django.contrib.auth.forms import AuthenticationForm
-
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, PasswordResetForm, SetPasswordForm
 
 
 class RegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'password1', 'password2', 'email']
+        fields = ['username', 'first_name', 'last_name',
+                  'password1', 'password2', 'email']
+
     def __init__(self, *args, **kwargs):
         super(UserCreationForm, self).__init__(*args, **kwargs)
 
-        for fieldname in ['username', 'password1', 'password2']: 
+        for fieldname in ['username', 'password1', 'password2']:
             self.fields[fieldname].help_text = None
+
 
 class CustomRegistrationForm(StyledFormMixin, forms.ModelForm):
     password1 = forms.CharField(widget=forms.PasswordInput)
     confirm_password = forms.CharField(widget=forms.PasswordInput)
+
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'password1', 'confirm_password', 'email']
-    
+        fields = ['username', 'first_name', 'last_name',
+                  'password1', 'confirm_password', 'email']
+
     def clean_email(self):
         email = self.cleaned_data.get('email')
-        email_exists = User.objects.filter(email = email).exists()
+        email_exists = User.objects.filter(email=email).exists()
 
         if email_exists:
-            raise forms.ValidationError("Email Already exits")
+            raise forms.ValidationError("Email already exists")
+
         return email
 
     def clean_password1(self):
@@ -38,28 +43,42 @@ class CustomRegistrationForm(StyledFormMixin, forms.ModelForm):
 
         if len(password1) < 8:
             errors.append('Password must be at least 8 character long')
+
+        if not re.search(r'[A-Z]', password1):
+            errors.append(
+                'Password must include at least one uppercase letter.')
+
         if not re.search(r'[a-z]', password1):
-            errors.append("Password must include at least one lower case letter")
+            errors.append(
+                'Password must include at least one lowercase letter.')
+
         if not re.search(r'[0-9]', password1):
-            errors.append("Password must include one number")
+            errors.append('Password must include at least one number.')
+
         if not re.search(r'[@#$%^&+=]', password1):
-            errors.append("Password must include one special character")
+            errors.append(
+                'Password must include at least one special character.')
+
         if errors:
             raise forms.ValidationError(errors)
+
         return password1
-    
-    def clean(self):
+
+    def clean(self):  # non field error
         cleaned_data = super().clean()
         password1 = cleaned_data.get('password1')
         confirm_password = cleaned_data.get('confirm_password')
 
         if password1 and confirm_password and password1 != confirm_password:
             raise forms.ValidationError("Password do not match")
+
         return cleaned_data
+
 
 class LoginForm(StyledFormMixin, AuthenticationForm):
     def __init__(self, *arg, **kwargs):
         super().__init__(*arg, **kwargs)
+
 
 class AssignRoleForm(StyledFormMixin, forms.Form):
     role = forms.ModelChoiceField(
@@ -67,12 +86,27 @@ class AssignRoleForm(StyledFormMixin, forms.Form):
         empty_label="Select a Role"
     )
 
-class createGroupForm(StyledFormMixin, forms.ModelForm):
+
+class CreateGroupForm(StyledFormMixin, forms.ModelForm):
     permissions = forms.ModelMultipleChoiceField(
-        queryset= Permission.objects.all(),
-        widget = forms.CheckboxSelectMultiple,
-        label = 'Assign Permission'
+        queryset=Permission.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+        label='Assign Permission'
     )
+
     class Meta:
         model = Group
         fields = ['name', 'permissions']
+
+
+class CustomPasswordChangeForm(StyledFormMixin, PasswordChangeForm):
+    pass
+
+
+class CustomPasswordResetForm(StyledFormMixin, PasswordResetForm):
+    pass
+
+
+class CustomPasswordResetConfirmForm(StyledFormMixin, SetPasswordForm):
+    pass
--- users/views.py
+++ users/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.models import User, Group
 from django.contrib.auth import login, logout
-from users.forms import CustomRegistrationForm, AssignRoleForm, createGroupForm
+from users.forms import CustomRegistrationForm, AssignRoleForm, CreateGroupForm, CustomPasswordChangeForm, CustomPasswordResetForm, CustomPasswordResetConfirmForm
+from django.contrib import messages
 from django.contrib import messages
 from users.forms import LoginForm
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.db.models import Prefetch
+from django.contrib.auth.views import LoginView, PasswordChangeView, PasswordResetView, PasswordResetConfirmView
+from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+
+# Create your views here.
+
+# Test for users
 
 
-
-#Test for users
 def is_admin(user):
     return user.groups.filter(name='Admin').exists()
 
-def is_organizer(user):
-    return user.groups.filter(name='Organizer').exists()
-
-def is_participant(user):
-    return user.groups.filter(name='Participant').exists()
 
 def sign_up(request):
     form = CustomRegistrationForm()
@@ -29,12 +30,14 @@ def sign_up(request):
             user.set_password(form.cleaned_data.get('password1'))
             user.is_active = False
             user.save()
-            messages.success(request, 'A confirmation mail sent. Please check your email')
+            messages.success(
+                request, 'A Confirmation mail sent. Please check your email')
             return redirect('sign-in')
+
         else:
             print("Form is not valid")
-            
     return render(request, 'registration/register.html', {"form": form})
+
 
 def sign_in(request):
     form = LoginForm()
@@ -46,12 +49,27 @@ def sign_in(request):
             return redirect('home')
     return render(request, 'registration/login.html', {'form': form})
 
+
+class CustomLoginView(LoginView):
+    form_class = LoginForm
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        return next_url if next_url else super().get_success_url()
+
+
+class ChangePassword(PasswordChangeView):
+    template_name = 'accounts/password_change.html'
+    form_class = CustomPasswordChangeForm
+
+
 @login_required
 def sign_out(request):
     if request.method == 'POST':
         logout(request)
         return redirect('sign-in')
-    
+
+
 def activate_user(request, user_id, token):
     try:
         user = User.objects.get(id=user_id)
@@ -61,52 +79,124 @@ def activate_user(request, user_id, token):
             return redirect('sign-in')
         else:
             return HttpResponse('Invalid Id or token')
+
     except User.DoesNotExist:
         return HttpResponse('User not found')
 
-@user_passes_test(is_admin, login_url='no-permission')   
+
+@user_passes_test(is_admin, login_url='no-permission')
 def admin_dashboard(request):
     users = User.objects.prefetch_related(
         Prefetch('groups', queryset=Group.objects.all(), to_attr='all_groups')
-        ).all()
-    print(users)
+    ).all()
+
+    # print(users)
+
     for user in users:
         if user.all_groups:
             user.group_name = user.all_groups[0].name
         else:
             user.group_name = 'No Group Assigned'
-
     return render(request, 'admin/dashboard.html', {"users": users})
 
-@user_passes_test(is_admin, login_url='no-permission')   
+
+@user_passes_test(is_admin, login_url='no-permission')
 def assign_role(request, user_id):
     user = User.objects.get(id=user_id)
-    form = AssignRoleForm
+    form = AssignRoleForm()
 
     if request.method == 'POST':
         form = AssignRoleForm(request.POST)
         if form.is_valid():
             role = form.cleaned_data.get('role')
-            user.groups.clear() #Remove old roles
+            user.groups.clear()  # Remove old roles
             user.groups.add(role)
             messages.success(request, f"User {user.username} has been assigned to the {role.name} role")
             return redirect('admin-dashboard')
+
     return render(request, 'admin/assign_role.html', {"form": form})
 
-@user_passes_test(is_admin, login_url='no-permission')   
+
+
+
+
+
+@user_passes_test(is_admin, login_url='no-permission')
 def create_group(request):
-    form = createGroupForm()
+    form = CreateGroupForm()
     if request.method == 'POST':
-        form = createGroupForm(request.POST)
+        form = CreateGroupForm(request.POST)
 
         if form.is_valid():
             group = form.save()
             messages.success(request, f"Group {group.name} has been created successfully")
             return redirect('create-group')
+
     return render(request, 'admin/create_group.html', {'form': form})
 
-@user_passes_test(is_admin, login_url='no-permission')   
+
+@user_passes_test(is_admin, login_url='no-permission')
 def group_list(request):
     groups = Group.objects.prefetch_related('permissions').all()
     return render(request, 'admin/group_list.html', {'groups': groups})
 
+
+class ProfileView(TemplateView):
+    template_name = 'accounts/profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+
+        context['username'] = user.username
+        context['email'] = user.email
+        context['name'] = user.get_full_name()
+
+        context['member_since'] = user.date_joined
+        context['last_login'] = user.last_login
+        return context
+
+
+class CustomPasswordResetView(PasswordResetView):
+    form_class = CustomPasswordResetForm
+    template_name = 'registration/reset_password.html'
+    success_url = reverse_lazy('sign-in')
+    html_email_template_name = 'registration/reset_email.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['protocol'] = 'https' if self.request.is_secure() else 'http'
+        context['domain'] = self.request.get_host()
+        print(context)
+        return context
+
+    def form_valid(self, form):
+        messages.success(
+            self.request, 'A Reset email sent. Please check your email')
+        return super().form_valid(form)
+
+
+class CustomPasswordResetConfirmView(PasswordResetConfirmView):
+    form_class = CustomPasswordResetConfirmForm
+    template_name = 'registration/reset_password.html'
+    success_url = reverse_lazy('sign-in')
+
+    def form_valid(self, form):
+        messages.success(
+            self.request, 'Password reset successfully')
+        return super().form_valid(form)
+
+
+""" 
+
+    Admin
+        - Sobkisui
+    Manager
+        - project
+        - task create
+    Employee
+        - Task read
+        - Task update
+    
+    Role Based Access Control (RBAC)
+"""
